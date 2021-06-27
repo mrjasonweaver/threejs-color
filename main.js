315727e90dfeddb1980c076319156ce224068110
mrjasonweaver <indyplanets@gmail.com>
2021-06-27 12:39:22 -0500

    Updating color variable

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-scene.background = new THREE.Color( 0xffffff ); // white
 const renderer = new THREE.WebGLRenderer({ alpha: true }); // adds canvas transparency
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+const white = new THREE.Color( 0xffffff );
+const black = new THREE.Color( 0x000000 );
+const red = new THREE.Color( 0xff0000 );
+const green = new THREE.Color( 0x00ff00 );
+const blue = new THREE.Color( 0x0000ff );
+
+let cubeColor = black;
+scene.background = white;
+
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-let color = 0x000000; // black
 scene.add( cube );
 
 camera.position.z = 5;
@@ -16,13 +23,13 @@ camera.position.z = 5;
 window.addEventListener("keyup", event => {
   switch(event.code) {
     case "KeyR":
-      color = 0xFF0000; // red
+      cubeColor = red;
       break;
     case "KeyG":
-      color = 0x00FF00; // green
+      cubeColor = green;
       break;
     case "KeyB":
-      color = 0x0000FF; // blue
+      cubeColor = blue;
       break;
   }
 }, true);
@@ -30,7 +37,7 @@ window.addEventListener("keyup", event => {
 
 const animate = () => {
   requestAnimationFrame( animate );
-  cube.material.color.set(color);
+  cube.material.color.set(cubeColor);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
   renderer.render( scene, camera );
