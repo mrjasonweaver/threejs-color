9847e57ef1058b7a28fc9938cb8a798cf4f5cd9e
mrjasonweaver <indyplanets@gmail.com>
2021-06-29 07:29:49 -0500

    Adds color tweening

--- main.js
+++ main.js
@@ -10,36 +10,60 @@ const red = new THREE.Color( 0xff0000 );
 const green = new THREE.Color( 0x00ff00 );
 const blue = new THREE.Color( 0x0000ff );
 
-let cubeColor = black;
 scene.background = white;
+
+let state = {
+  cubeColor: black,
+  previousTweenColor: black,
+  nextTweenColor: black,
+  alphaUnit: 0,
+  isTweenRunning: false
+}
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial();
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
-
+cube.material.color.set(state.cubeColor);
 camera.position.z = 5;
 
 window.addEventListener("keyup", event => {
   switch(event.code) {
     case "KeyR":
-      cubeColor = red;
+      tweenColors(red);
       break;
     case "KeyG":
-      cubeColor = green;
+      tweenColors(green);
       break;
     case "KeyB":
-      cubeColor = blue;
+      tweenColors(blue);
       break;
   }
 }, true);
 
+const tweenColors = color => {
+  state = {
+    ...state,
+    previousTweenColor: state.cubeColor,
+    nextTweenColor: color,
+    alphaUnit: 0,
+    isTweenRunning: true
+  }
+}
 
 const animate = () => {
   requestAnimationFrame( animate );
-  cube.material.color.set(cubeColor);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+
+  if (state.isTweenRunning && !state.previousTweenColor.equals(state.nextTweenColor) ) {
+    state.alphaUnit = +(state.alphaUnit + 0.01).toFixed(2);
+    state.cubeColor = state.cubeColor.lerpColors(state.previousTweenColor, state.nextTweenColor, state.alphaUnit);
+    cube.material.color.set(state.cubeColor);
+  } else {
+    state.isTweenRunning = false;
+  }
+
   renderer.render( scene, camera );
 };
 
